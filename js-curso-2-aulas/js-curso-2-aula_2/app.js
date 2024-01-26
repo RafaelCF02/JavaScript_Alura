54801d2ccef1b4e264b1ca3b6b41dc508ecbdddf
RafaelCF02 <Rcf_1002@proton.me>
2024-01-26 11:18:22 -0300

    Atualização da aula 2

--- js-curso-2-aulas/js-curso-2-aula_2/app.js
+++ js-curso-2-aulas/js-curso-2-aula_2/app.js
@@ -1,22 +1,56 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function mensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+mensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1','Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas':'tentativa';
+        let mensagemTentativas = `Voce descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p',mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
+    else{
+        if(chute>numeroSecreto){
+            exibirTextoNaTela('p','O numero é menor que o chute');
+        }
+        else{
+            exibirTextoNaTela('p','O numero secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
 
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value =''
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    mensagemInicial();
+    tentativas = 1;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+
+}
 
 
 
